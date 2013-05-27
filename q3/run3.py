159221ae2a2786576c0e279e41bc10088f121136
Andrew Dupree <ajdupree@drink-0.Stanford.EDU>
2013-05-27 11:10:03 -0700

    fixed some things in q3 run

--- q3/run3.py
+++ q3/run3.py
@@ -4,19 +4,19 @@ import os
 import sys
 import re
 ###################
-### parts 1 and 2 #
+### part 1       ##
 ###################
 
 ##print "Starting Part 1"
 ##applications = ["blackscholes", "fluidanimate", "streamcluster", "swaptions", "art"]
 ##for app in applications:
-##	for cores in range(1,9):
-##		command = "zsim.sh -a "
-##		command += str(app)
-##		command += " -c "
-##		command += str(cores)
-##		print "Running job ", command
-##		os.system(command)
+##  for cores in range(1,9):
+##    command = "zsim.sh -a "
+##    command += str(app)
+##    command += " -c "
+##    command += str(cores)
+##    print "Running job ", command
+##    os.system(command)
 ##
 ##print "finished dispatching all jobs"
 
@@ -31,32 +31,31 @@ coreType = ["wide", "narrow"]
 fileOut = open("q3_out", "w");
 
 for width in coreType:
-	corelist = range(1,9) if width == "wide" else range(1,17)
-	
-	for cores in corelist:
-		print "RUNNING", width, cores
-		
-		for size in range(256, 32768+256, 256):
-			os.chdir("junk")
-			call([root, "-S","-a", "blackscholes", "-f" "1700", "-b", width,"-c", str(cores), "--l2size", str(size)], stdout=w, stderr=w)
-			os.chdir("../")
+  corelist = range(1,9) if width == "wide" else range(1,17)
+  
+  for cores in corelist:
+    print "RUNNING", width, cores
+    
+    for size in range(256, 32768+256, 256):
+      os.chdir("junk")
+      call([root, "-S","-a", "blackscholes", "-f" "1700", "-b", width,"-c", str(cores), "--l2size", str(size)], stdout=w, stderr=w)
+      os.chdir("../")
 
-			out = os.read(r,1024)
-			found = re.search("error", out)
-			
-			if found != None or size==32768:
-					sizeout = size-256 if size != 32768 else size
-					out = width + " " + str(cores) + " " + str(sizeout)
-					print out
-					fileOut.write(out)
-					fileOut.write('\n')
-					fileOut.flush()
+      out = os.read(r,1024)
+      found = re.search("error", out)
+      
+      if found != None or size==32768:
+          sizeout = size-256 if found!=None else size
+          out = width + " " + str(cores) + " " + str(sizeout)
+          print out
+          fileOut.write(out)
+          fileOut.write('\n')
+          fileOut.flush()
 
-					os.chdir("data")
-					call([root, "-B", "-a", "blackscholes", "-f" "1700", "-b", width,"-c", str(cores), "--l2size", str(sizeout)])
-					os.chdir("../")
-					
-					break
+          os.chdir("data")
+          call([root, "-B" "-a", "blackscholes", "-f" "1700", "-b", width,"-c", str(cores), "--l2size", str(sizeout)])
+          os.chdir("../")
+          break;
 
 fileOut.close()
 
--- run.py
+++ run.py
@@ -4,19 +4,19 @@ import os
 import sys
 import re
 ###################
-### parts 1 and 2 #
+### part 1       ##
 ###################
 
 ##print "Starting Part 1"
 ##applications = ["blackscholes", "fluidanimate", "streamcluster", "swaptions", "art"]
 ##for app in applications:
-##	for cores in range(1,9):
-##		command = "zsim.sh -a "
-##		command += str(app)
-##		command += " -c "
-##		command += str(cores)
-##		print "Running job ", command
-##		os.system(command)
+##  for cores in range(1,9):
+##    command = "zsim.sh -a "
+##    command += str(app)
+##    command += " -c "
+##    command += str(cores)
+##    print "Running job ", command
+##    os.system(command)
 ##
 ##print "finished dispatching all jobs"
 
@@ -27,20 +27,37 @@ import re
 r,w = os.pipe();
 
 root = "/afs/ir/class/ee282/spring13_pa2/bin/zsim.sh"
+coreType = ["wide", "narrow"]
+fileOut = open("q3_out", "w");
 
-coreType = "wide"
-numCores = 8
-l2size = 32768
+for width in coreType:
+  corelist = range(1,9) if width == "wide" else range(1,17)
+  
+  for cores in corelist:
+    print "RUNNING", width, cores
+    
+    for size in range(256, 32768+256, 256):
+      os.chdir("junk")
+      call([root, "-S","-a", "blackscholes", "-f" "1700", "-b", width,"-c", str(cores), "--l2size", str(size)], stdout=w, stderr=w)
+      os.chdir("../")
 
-call([root, "-S","-a", "blackscholes", "-f" "1700", "-b", coreType, "-c", str(numCores), "--l2size", str(l2size)], stdout=w, stderr=w);
+      out = os.read(r,1024)
+      found = re.search("error", out)
+      
+      if found != None or size==32768:
+          sizeout = size-256 if found!=None else size
+          out = width + " " + str(cores) + " " + str(sizeout)
+          print out
+          fileOut.write(out)
+          fileOut.write('\n')
+          fileOut.flush()
 
-out = os.read(r,1024)
-found = re.search("error", out)
+          os.chdir("data")
+          call([root, "-B" "-a", "blackscholes", "-f" "1700", "-b", width,"-c", str(cores), "--l2size", str(sizeout)])
+          os.chdir("../")
+          break;
 
-if found == None:
-	print "success"
-else:
-	print "fail"
+fileOut.close()
 
 
 
